241d582e2344ce962ec71a6a6a7482c66b6bc36c
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2026-01-25 19:17:05 +0530

    Evaluation2

--- MockEvaluation2/backend/index.js
+++ MockEvaluation2/backend/index.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const cors=require("cors");
 require("dotenv").config();
-const supabaseg
+const supabase = require("./supabase");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/register",asyn(req,res)=>{
+    const{full_name || !email ||!phone}=req.body;
+    if(!full_name || !email || !phone){
+        return res.status(400).json({error:"All fields are requires"})
+    }
+    const {data: existingCustomer} = await supabase
+    .from("customers")
+    .select("*")
+    .eq("emial",email)
+    .single();
+
+    
+    
+})
+app.listen(3000,()=>{
+    console.log("Server running on port 3000")
+})
